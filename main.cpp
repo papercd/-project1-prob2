e2fc65ba1e7fd1aae77092f56c56b4ed9258e227
papercd <hpptknk10@hotmail.co.kr>
2023-09-29 00:13:21 +0900

    basic outline of all objects added

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 
 using namespace std; 
 
+int OptionInputChecker(int from, int to);
 
 class MainMenu {
 	int input = 0; 
 	public:
 
 		void showMenu() {
+			cout << "- Main menu -\n";
 			cout << "1. Insertion\n";
 			cout << "2. Search\n";
 			cout << "3. Sorting Option\n";
@@ -18,15 +20,7 @@ class MainMenu {
 		void getInput() {
 			//get correct input from user. error handling should be implemented. 
 			cout << "> ";
-			cin >> input; 
-			while ((!cin) || (input <=0 || input > 5))
-			{
-				cin.clear();
-				cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-				cout << "please input an integer from 1 to 4." << endl << "> ";
-				cin >> input; 
-			}
+			input = OptionInputChecker(1, 4);
 		}
 
 		int returnInput() {
@@ -77,18 +71,55 @@ class Inserter {
 };
 
 class Searcher {
-
-
-
+	public:
+		void search() {
+			int option;
+			cout << "- Search -" << endl; 
+			cout << "1. Search by name" << endl; 
+			cout << "2. Search by student ID (10 numbers)" << endl;
+			cout << "3. Search by admission year (4 numbers)" << endl;
+			cout << "4. Search by department name" << endl;
+			cout << "5. List all" << endl;
+			cout << "> ";
+			option = OptionInputChecker(1, 5);
+		}
 };
 
+class Sorter {
+	int sortOption = 1;
+	public:
+		void ChooseSortingOption() {
+			cout << "- Sorting Option -" << endl;
+			cout << "1. Sort by Name" << endl;
+			cout << "2. Sort by Name" << endl;
+			cout << "3. Sort by Student ID" << endl;
+			cout << "4. Sort by Department name" << endl;
+			cout << "> ";
+			sortOption = OptionInputChecker(1, 4);
+		}
+};
+
+int OptionInputChecker(int from, int to) {
+	int input; 
+	cin >> input; 
+	while ((!cin) || (input < from || input > to))
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+		cout << "please input a number from " << from<< " to "<< to << endl << "> ";
+		cin >> input;
+	}
+	return input; 
+}
 
 
 int main(int argc, char*argv[]) {
 
 	if (argc != 2)
 	{
-		cout << "detected wrong format of input arguements. Please input a filename after executable file : ";
+		//error when input arguement is incorrect.
+		cout << "detected wrong format of input arguements. Please input a .txt filename after executable file ." << endl;
 		return 0;
 	}
 
@@ -96,6 +127,7 @@ int main(int argc, char*argv[]) {
 	MainMenu menu;
 	Inserter insert; 
 	Searcher search; 
+	Sorter sort;
 	
 	while (menu.returnInput()!=4)
 	{
@@ -107,10 +139,10 @@ int main(int argc, char*argv[]) {
 			insert.insertInfo();
 			break;
 		case 2:
-
+			search.search();
 			break; 
 		case 3:
-
+			sort.ChooseSortingOption();
 			break;
 		case 4: 
 			return 0;
